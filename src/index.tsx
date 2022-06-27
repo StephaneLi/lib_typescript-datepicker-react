19fdc4cd692ccf95ffb74d17af4993a502c5c543
Stéphane <stephane1920@gmail.com>
2022-06-27 17:27:06 +0200

    update demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import DatePicker from './lib/DatePicker';
+import './style.css';
 
 
 const root = ReactDOM.createRoot(
@@ -9,6 +10,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <DatePicker label='date picker' name='date-picker' error={true} />
+    <div className="demo">
+      <DatePicker label='date picker' name='date-picker' />
+    </div>
   </React.StrictMode>
 )
